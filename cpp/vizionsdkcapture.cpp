f669bf62618df9f3566df0a8014ff4ccead56686
selena <selena.chen@technexion.com>
2025-07-04 11:04:08 +0800

    feat(VizionSDKCapture): Add video window with brightness control viz keyboard

--- cpp/vizionsdkcapture.cpp
+++ cpp/vizionsdkcapture.cpp
@@ -1,3 +1,13 @@
+/*
+vizionsdkcapture.cpp - Demonstrates how to use vizionsdk to control ISP camera brightness
+
+Opens a video window with live camera stream.In the window, keys can be pressed to
+increase or decrease the brightness.
+u : increase 
+d : decrease 
+q: stop playing video and quit program
+*/
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -9,86 +19,140 @@
 
 #include "VizionSDK.h"
 #include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
+
+void draw_label(cv::Mat& img, const std::string& text, const cv::Point& pos) {
+    int font_face = cv::FONT_HERSHEY_SIMPLEX;
+    double scale = 0.5;
+    cv::Scalar color = cv::Scalar::all(255);
+    int thickness = cv::FILLED;
+    int margin = 5;
+    int baseline = 0;
+    cv::Size textSize = cv::getTextSize(text, font_face, scale, thickness, &baseline);
+
+    int end_x = pos.x + textSize.width + margin;
+    int end_y = pos.y - textSize.height - margin;
+    int start_x = pos.x - margin;
+    int start_y = pos.y + margin;
+
+    cv::rectangle(img, cv::Point(start_x, start_y), cv::Point(end_x, end_y), cv::Scalar(0, 0, 0),
+                  thickness);
+    cv::putText(img, text, pos, font_face, scale, color, 1, cv::LINE_AA);
+}
 
 int main() {
-    std::cout << "Opencv Version: " << CV_VERSION << std::endl;
-
-    // get the camera list
+    // Get the camera list
     std::vector<std::string> dev_list;
-    int can_num = VxDiscoverCameraDevices(dev_list);
-    if (can_num == 0) {
+    int cam_num = VxDiscoverCameraDevices(dev_list);
+    if (cam_num == 0) {
         std::cout << "Cannot Find any Cameras!" << std::endl;
         return -1;
     }
 
+    // Print the device list
+    std::cout << "Device List:" << std::endl;
+    for (auto device : dev_list) {
+        std::cout << device << std::endl;
+    }
+
+    // Choose the first camera device
     int dev_idx = 0;
-    // initial the index 0 camera 
     std::cout << "seletected [" << dev_idx << "] device" << std::endl;
     std::cout << "device name: " << dev_list[dev_idx] << std::endl;
     auto cam = VxInitialCameraDevice(dev_idx);
 
+    // Open camera
     VxOpen(cam);
 
-    // get format to min size for mjpg format
+    // Get format
     std::vector<VxFormat> fmt_list;
     VxGetFormatList(cam, fmt_list);
+
     int min_width = 1920;
     int min_height = 1080;
+    VxFormat mjpg_fmt;
     for (auto fmt : fmt_list) {
-        // find MJPG smallest size format
+        // Find the minimum resolution in MJPG format
         if (fmt.format == VX_IMAGE_FORMAT::MJPG &&
             fmt.width * fmt.height < min_width * min_height) {
             min_width = fmt.width;
             min_height = fmt.height;
+            mjpg_fmt = fmt;
         }
     }
 
-    // capture the video and display with cv2.imshow()
+    // Set format
+    if (VxSetFormat(cam, mjpg_fmt) == 0) {
+        std::cout << "Set Capture Format Success!" << std::endl;
+    } else {
+        std::cout << "Set Capture Format Failed!" << std::endl;
+    }
+
+    std::cout << "Video resolution: " << min_width << " x " << min_height << std::endl;
+
+    // Capture image with opencv VideoCapture API
     cv::VideoCapture cap;
-    #ifdef _WIN32
-        cap = cv::VideoCapture(dev_idx, cv::CAP_DSHOW);
-    #else
-        cap = cv::VideoCapture(dev_idx);
-    #endif
+#ifdef _WIN32
+    cap = cv::VideoCapture(dev_idx, cv::CAP_DSHOW);
+#else
+    cap = cv::VideoCapture(dev_idx);
+#endif
+
     cap.set(cv::CAP_PROP_FRAME_WIDTH, min_width);
     cap.set(cv::CAP_PROP_FRAME_HEIGHT, min_height);
-    cv::Mat frameBefore;
-    cap >> frameBefore;
-    cv::imshow("Before setting", frameBefore);
-    cv::waitKey(0);
-    cv::destroyAllWindows();
+
+    // Get the brightness range
+    int min_brightness, max_brightness, step, def;
+    VxGetISPImageProcessingRange(cam, VX_ISP_IMAGE_PROPERTIES::ISP_IMAGE_BRIGHTNESS, min_brightness,
+                                 max_brightness, step, def);
+    std::cout << "Brightness min: " << min_brightness << ", max: " << max_brightness
+              << ", step: " << step << ", default: " << def << std::endl;
+
+    // Get the brightness
+    int brightness, flag;
+    VxGetISPImageProcessing(cam, VX_ISP_IMAGE_PROPERTIES::ISP_IMAGE_BRIGHTNESS, brightness, flag);
+    std::cout << "Intial ISP brightness: " << brightness << ", flag: " << flag << std::endl;
+
+    // Set the brightness
+    std::cout << "Playing video with brightness " << brightness << ", press q in windows to exit..."
+              << std::endl;
+
+    cv::Mat frame;
+    while (true) {
+        bool ret = cap.read(frame);
+        if (ret) {
+            std::string brightness_label = "brightness: " + std::to_string(brightness);
+            std::string label_str = "u: increase, d: decrease, q: exit";
+            draw_label(frame, brightness_label, cv::Point(20, 20));
+            draw_label(frame, label_str, cv::Point(20, 40));
+            cv::imshow("Adjust brightness", frame);
+            int key = cv::waitKey(1) & 0xFF;
+            if (key == 'u') {
+                if (brightness < max_brightness) {
+                    brightness = brightness + step;
+                }
+                VxSetISPImageProcessing(cam, VX_ISP_IMAGE_PROPERTIES::ISP_IMAGE_BRIGHTNESS,
+                                        brightness);
+                std::cout << "Set brightness to: " << brightness << std::endl;
+            }
+            if (key == 'd') {
+                if (brightness > min_brightness) {
+                    brightness = brightness - step;
+                }
+                VxSetISPImageProcessing(cam, VX_ISP_IMAGE_PROPERTIES::ISP_IMAGE_BRIGHTNESS,
+                                        brightness);
+                std::cout << "Set brightness to: " << brightness << std::endl;
+            }
+            if (key == 'q') {
+                break;
+            }
+        } else {
+            std::cout << "Can't receive frame (stream end?). Exiting ..." << std::endl;
+            break;
+        }
+    }
+
     cap.release();
-
-    // get the brightness
-    long value;
-    int flag;
-    VxGetUVCImageProcessing(cam, VX_UVC_IMAGE_PROPERTIES::UVC_IMAGE_BRIGHTNESS, value, flag);
-    std::cout << "UVC brightness: " << value << std::endl;
-
-    // set the brightness
-    int result;
-    result = VxSetUVCImageProcessing(cam, VX_UVC_IMAGE_PROPERTIES::UVC_IMAGE_BRIGHTNESS, 16, flag);
-    std::cout << "Set UVC brightness return code: " << result << std::endl;
-
-    // get the brightness
-    VxGetUVCImageProcessing(cam, VX_UVC_IMAGE_PROPERTIES::UVC_IMAGE_BRIGHTNESS, value, flag);
-    std::cout << "After setting UVC brightness: " << value << std::endl;
-
-    // capture the video after setting the property and display with cv2.imshow()
-    #ifdef _WIN32
-        cap = cv::VideoCapture(dev_idx, cv::CAP_DSHOW);
-    #else
-        cap = cv::VideoCapture(dev_idx);
-    #endif
-    cap.set(cv::CAP_PROP_FRAME_WIDTH, min_width);
-    cap.set(cv::CAP_PROP_FRAME_HEIGHT, min_height);
-    cv::Mat frameAfter;
-    cap >> frameAfter;
-    cv::imshow("After setting", frameAfter);
-    cv::waitKey(0);
-    cv::destroyAllWindows();
-    cap.release();
-
     VxClose(cam);
-	return 0;
+    return 0;
 }
--- python/pyvizionsdkcapture.py
+++ python/pyvizionsdkcapture.py
@@ -1,88 +1,123 @@
+"""
+pyvizionsdkcapture.py - Demonstrates how to use pyvizionsdk to control ISP camera brightness
+
+Opens a video window with live camera stream.  In the window, keys can be pressed to
+increase or decrease the brightness.
+
+ u: increase
+ d: decrease
+ q: stop playing video and quit program
+"""
+
 import pyvizionsdk
-from pyvizionsdk import VX_UVC_IMAGE_PROPERTIES, VX_IMAGE_FORMAT
+from pyvizionsdk import VX_ISP_IMAGE_PROPERTIES, VX_IMAGE_FORMAT
 
 import cv2
 import numpy as np
 
 import platform
 
-# get the camera list
-result, camera_list = pyvizionsdk.VxDiscoverCameraDevices()
-print("Return code:", result)
-print("Discovered cameras:", camera_list)
+def __draw_label(img, text, pos):
+    font_face = cv2.FONT_HERSHEY_SIMPLEX
+    scale = 0.5
+    color = (255, 255, 255)
+    thickness = cv2.FILLED
+    margin = 5
+    txt_size = cv2.getTextSize(text, font_face, scale, thickness)
 
-# print camera_list
+    end_x = pos[0] + txt_size[0][0] + margin
+    end_y = pos[1] - txt_size[0][1] - margin
+    start_x = pos[0] - margin
+    start_y = pos[1] + margin
+
+    cv2.rectangle(img, (start_x, start_y), (end_x, end_y), (0,0,0), thickness)
+    cv2.putText(img, text, pos, font_face, scale, color, 1, cv2.LINE_AA)
+
+# Get the camera list
+result, camera_list = pyvizionsdk.VxDiscoverCameraDevices()
+if result < 1:
+    print('No cameras discovered.')
+    quit()
+
+# Print camera_list
+print("Device List:\n")
 for camera in camera_list:
     print(camera)
 
-# initialize camera device
+# Choose the first camera device
 idx = 0
 camera = pyvizionsdk.VxInitialCameraDevice(idx)
 
-# open camera
+# Open camera
 result = pyvizionsdk.VxOpen(camera)
 print("Open camera return code:", result)
 
-# get format
+# Get format
 result, format_list = pyvizionsdk.VxGetFormatList(camera)
+print("Get format return code:", result)
+
 mjpg_format = None
 min_resolution = float('inf')
 for format in format_list:
-    # get mjpg format and minimum resolution
+    # Find the minimum resolution in MJPG format
     if format.format == VX_IMAGE_FORMAT.VX_IMAGE_FORMAT_MJPG:
         resolution = format.width * format.height
         if resolution < min_resolution:
             min_resolution = resolution
             mjpg_format = format
-print("Return code:", result)
 
-# set format
+# Set format
 result = pyvizionsdk.VxSetFormat(camera, mjpg_format)
 
 min_width = mjpg_format.width
 min_height = mjpg_format.height
 
-# captured by opencv
+print(f"Video resolution: ", min_width, "x", min_height)
+
+# Capture image with opencv VideoCapture API
 if platform.system() == 'Windows':
     cap = cv2.VideoCapture(idx, cv2.CAP_DSHOW)
 else:
     cap = cv2.VideoCapture(idx)
+
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, min_width)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, min_height)
-# Capture frame
-ret, frame = cap.read()
-cv2.imshow('Before setting', frame)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-cap.release()
 
-# get the brightness
-result, brightness, flag = pyvizionsdk.VxGetUVCImageProcessing(camera, VX_UVC_IMAGE_PROPERTIES.UVC_IMAGE_BRIGHTNESS)
-print("UVC brightness:", brightness)
-print("Flag:", flag)
-print("Return code:", result)
+# Get the brightness range
+result, min_brightness, max_brightness, step, def_brightness = pyvizionsdk.VxGetISPImageProcessingRange(camera, VX_ISP_IMAGE_PROPERTIES.ISP_IMAGE_BRIGHTNESS)
+print(f"Brightness min: {min_brightness}, max: {max_brightness}, step: {step}, default: {def_brightness}, return code: {result}")
 
-# set the brightness
-result = pyvizionsdk.VxSetUVCImageProcessing(camera, VX_UVC_IMAGE_PROPERTIES.UVC_IMAGE_BRIGHTNESS, 16, 0)
-print("Set UVC brightness return code:", result)
+# Get the brightness
+result, brightness, flag = pyvizionsdk.VxGetISPImageProcessing(camera, VX_ISP_IMAGE_PROPERTIES.ISP_IMAGE_BRIGHTNESS)
+print(f"Initial ISP brightness: {brightness}, flag: {flag}, return code: {result}")
 
-# get the brightness
-result, brightness, flag = pyvizionsdk.VxGetUVCImageProcessing(camera, VX_UVC_IMAGE_PROPERTIES.UVC_IMAGE_BRIGHTNESS)
-print("After setting UVC brightness:", brightness)
-print("Flag:", flag)
-print("Return code:", result)
+# Set the brightness
+print(f"Playing video with brightness {brightness}, press q in window to exit...")
 
-# captured by opencv
-if platform.system() == 'Windows':
-    cap = cv2.VideoCapture(idx, cv2.CAP_DSHOW)
-else:
-    cap = cv2.VideoCapture(idx)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, min_width)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, min_height)
-# Capture frame
-ret, frame = cap.read()
-cv2.imshow('After setting', frame)
-cv2.waitKey(0)
+# Capture video and display
+while True:
+    ret, frame = cap.read()
+    if ret:
+        __draw_label(frame, f"brightness: {brightness}", (20,20))
+        __draw_label(frame, "u: increase, d: decrease, q: exit", (20,40))
+        cv2.imshow(f"Adjust brightness", frame)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('u'):
+            if brightness < max_brightness:
+                brightness = brightness + step
+            result = pyvizionsdk.VxSetISPImageProcessing(camera, VX_ISP_IMAGE_PROPERTIES.ISP_IMAGE_BRIGHTNESS, brightness)
+            print(f"Set brightness to: {brightness}")
+        if key == ord('d'):
+            if brightness > min_brightness:
+                brightness = brightness - step
+            result = pyvizionsdk.VxSetISPImageProcessing(camera, VX_ISP_IMAGE_PROPERTIES.ISP_IMAGE_BRIGHTNESS, brightness)
+            print(f"Set brightness to: {brightness}")
+        if key == ord('q'):
+            break
+    else:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break       
+
 cap.release()
 cv2.destroyAllWindows()
 
